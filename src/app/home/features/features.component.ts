998a62ae4a72a173dd2e554e727850db7215fc24
Chris Griffing <cmgriffing@gmail.com>
2020-02-22 11:40:24 -0800

    feat(home): home page scaffolding

---
+++ src/app/home/features/features.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-features',
+  templateUrl: './features.component.html',
+  styleUrls: ['./features.component.scss']
+})
+export class FeaturesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/home/media/media.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-media',
+  templateUrl: './media.component.html',
+  styleUrls: ['./media.component.scss']
+})
+export class MediaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
